526a3a1d285a8f925e89e477cd377f56bf760090
Ricardo da Verdade Silva <ricardovsilva@msn.com>
2014-06-27 18:01:21 -0300

    Implemented GetStaticRecommendation

--- RecomendationSystem/RecomendationConsole/Program.cs
+++ RecomendationSystem/RecomendationConsole/Program.cs
@@ -11,6 +11,7 @@ namespace RecomendationConsole
         static void Main(string[] args)
         {
             var productRecommendations = Dictionary<Product, List<Recommendation>>();
+            productRecommendations = GetStaticProductRecommendations();
         }
 
         /// <summary>
@@ -19,7 +20,43 @@ namespace RecomendationConsole
         /// <returns>Returns list of recommendation with some values filled.</returns>
         public static Dictionary<Product, List<Recommendation>> GetStaticProductRecommendations()
         {
+            var recommendation = new List<Recommendation>();
+            var productRecommendations = new Dictionary<Product, List<Recommendation>>();
 
+            recommendation.Add(new Recommendation() { Name = "Wile E Coyote", Rating = 4.5 });
+            recommendation.Add(new Recommendation() { Name = "Bugs Bunny", Rating = 2.5 });
+            recommendation.Add(new Recommendation() { Name = "Elmer Fudd", Rating = 5.0 });
+            recommendation.Add(new Recommendation() { Name = "Foghorn Leghorn", Rating = 2.0 });
+            productRecommendations.Add("ACME Industrial Rocket Pack", recommendation);
+
+            recommendation = new List<Recommendation>();
+            recommendation.Add(new Recommendation() { Name = "Wile E Coyote", Rating = 5.0 });
+            recommendation.Add(new Recommendation() { Name = "Bugs Bunny", Rating = 3.5 });
+            recommendation.Add(new Recommendation() { Name = "Elmer Fudd", Rating = 1.0 });
+            recommendation.Add(new Recommendation() { Name = "Foghorn Leghorn", Rating = 3.5 });
+            recommendation.Add(new Recommendation() { Name = "Daffy Duck", Rating = 1.0 });
+            productRecommendations.Add("ACME Super Sling Shot", recommendation);
+
+            recommendation = new List<Recommendation>();
+            recommendation.Add(new Recommendation() { Name = "Wile E Coyote", Rating = 1.0 });
+            recommendation.Add(new Recommendation() { Name = "Bugs Bunny", Rating = 3.5 });
+            recommendation.Add(new Recommendation() { Name = "Elmer Fudd", Rating = 5.0 });
+            recommendation.Add(new Recommendation() { Name = "Foghorn Leghorn", Rating = 4.0 });
+            recommendation.Add(new Recommendation() { Name = "Daffy Duck", Rating = 4.0 });
+            productRecommendations.Add("ACME X-Large Catapult", recommendation);
+
+            recommendation = new List<Recommendation>();
+            recommendation.Add(new Recommendation() { Name = "Bugs Bunny", Rating = 3.5 });
+            recommendation.Add(new Recommendation() { Name = "Elmer Fudd", Rating = 4.0 });
+            recommendation.Add(new Recommendation() { Name = "Foghorn Leghorn", Rating = 5.0 });
+            recommendation.Add(new Recommendation() { Name = "Daffy Duck", Rating = 2.5 });
+            productRecommendations.Add("ACME Super Glue", recommendation);
+
+            recommendation = new List<Recommendation>();
+            recommendation.Add(new Recommendation() { Name = "Bugs Bunny", Rating = 3.5 });
+            recommendation.Add(new Recommendation() { Name = "Elmer Fudd", Rating = 4.0 });
+            recommendation.Add(new Recommendation() { Name = "Foghorn Leghorn", Rating = 5.0 });
+            productRecommendations.Add("ACME Jet Powered Roller Skates", recommendation);
         }
     }
 }
