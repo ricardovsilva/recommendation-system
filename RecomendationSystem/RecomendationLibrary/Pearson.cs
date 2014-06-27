47038b001488cbcaf12a398c26dbc0a113eb6eba
Ricardo da Verdade Silva <ricardovsilva@msn.com>
2014-06-27 18:09:29 -0300

    Created class Pearson
    This class will contains the Pearson Correlation Score. As Joel Cogley
    said in his article, the algorithm is:
    "first finds the reviewers that reviewed both products. It then
    computes the sums and the squared sums of the ratings for the two
    products, and
    computes the sum of the reviews of the products. Finally,
    it uses these results to compute the Pearson Correlation Score."

---
+++ RecomendationSystem/RecomendationLibrary/Pearson.cs
@@ -0,0 +1,20 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Pearson.cs">
+//     Copyright (c) Ricardo da Verdade Silva. All rights reserved.
+// </copyright>
+// <author>Ricardo da Verdade Silva</author>
+//-----------------------------------------------------------------------
+namespace RecomendationLibrary
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// This class contains calculations methods of Pearson.
+    /// </summary>
+    public abstract class Pearson
+    {
+    }
+}
