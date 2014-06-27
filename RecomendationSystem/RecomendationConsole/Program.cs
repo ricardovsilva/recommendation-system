2ffde28ecde2fc5938e9f54d55d732a9c0671596
Ricardo da Verdade Silva <ricardovsilva@msn.com>
2014-06-27 17:52:56 -0300

    Created method GetStaticRecommendation
    This method have a lot of recommendation hardcoded, and it returns this.

--- RecomendationSystem/RecomendationConsole/Program.cs
+++ RecomendationSystem/RecomendationConsole/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using RecomendationLibrary;
 
 namespace RecomendationConsole
 {
@@ -9,6 +10,16 @@ namespace RecomendationConsole
     {
         static void Main(string[] args)
         {
+            var productRecommendations = Dictionary<Product, List<Recommendation>>();
+        }
+
+        /// <summary>
+        /// This method returns one list of recommendation with values filled.
+        /// </summary>
+        /// <returns>Returns list of recommendation with some values filled.</returns>
+        public static List<Recommendation> GetStaticRecommendationList()
+        {
+
         }
     }
 }
