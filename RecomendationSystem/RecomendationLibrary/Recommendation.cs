b908d5a4d2c8777cdc3f09c8b309be8a10b0691f
Ricardo da Verdade Silva <ricardovsilva@msn.com>
2014-06-27 17:35:47 -0300

    Created class Recommendation.cs
    At this moment, nothing was written in this class yet, it's just
    created.

---
+++ RecomendationSystem/RecomendationLibrary/Recommendation.cs
@@ -0,0 +1,20 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Recommendation.cs">
+//     Copyright (c) Ricardo da Verdade Silva. All rights reserved.
+// </copyright>
+// <author>Ricardo da Verdade Silva</author>
+//-----------------------------------------------------------------------
+namespace RecomendationLibrary
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// This class represents recommendation of user.
+    /// </summary>
+    public class Recommendation
+    {
+    }
+}
